a98360613644ec41b6ab73ce482ea341dc120ada
Elliot Berman <elliotjberman@gmail.com>
2018-04-21 20:22:07 -0400

    second commit

---
+++ src/main.cpp
@@ -0,0 +1,119 @@
+/* Deeply indebted to the project
+ * Mini Untztrument, Arduino Style by gdsports
+ *
+ * Repo here: https://github.com/gdsports/miniuntz
+ */
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Trellis.h>
+#include <MIDIUSB.h>
+
+#define LED     LED_BUILTIN // Pin for heartbeat LED (shows code is working)
+#define CHANNEL 1           // MIDI channel number
+
+Adafruit_Trellis trellis;
+
+uint8_t       heart        = 0;  // Heartbeat LED counter
+unsigned long prevReadTime = 0L; // Keypad polling timer
+
+uint8_t note[] = {
+  48, 49, 50, 51,
+  44, 45, 46, 47,
+  40, 41, 42, 43,
+  36, 37, 38, 39
+};
+// Wires would be here on the Trellis
+
+int findIndex(int x) {
+  int row = 12 - (x / 4);
+  int column = x % 4;
+  return row * 4 + column;
+}
+
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, (byte)(0x90 | channel), pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, (byte)(0x80 | channel), pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+// First parameter is the event type (0x0B = control change).
+// Second parameter is the event type, combined with the channel.
+// Third parameter is the control number number (0-119).
+// Fourth parameter is the control value (0-127).
+// Currently not in use
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, (byte) (0xB0 | channel), control, value};
+  MidiUSB.sendMIDI(event);
+}
+
+void startupAnimation(Adafruit_Trellis t) {
+  int speed = 45;
+  for (int i=0; i<16; ++i) {
+    t.setLED(i);
+    t.writeDisplay();
+    delay(speed);
+  }
+  for (int i=0; i<16; ++i) {
+    t.clrLED(i);
+    t.writeDisplay();
+    delay(speed);
+  }
+}
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  trellis.begin(0x70); // Pass I2C address
+
+  startupAnimation(trellis);
+
+  trellis.clear();
+  trellis.writeDisplay();
+
+}
+
+void loop() {
+  unsigned long t = millis();
+  if((t - prevReadTime) >= 20L) { // 20ms = min Trellis poll time
+    if(trellis.readSwitches()) {  // Button state change?
+
+      for(uint8_t i=0; i<16; i++) { // For each button...
+        if(trellis.justPressed(i)) {
+          noteOn(CHANNEL, note[i], 127);
+
+        } else if(trellis.justReleased(i)) {
+          noteOn(CHANNEL, note[i], 0);
+        }
+      }
+    }
+
+    // Read midi in
+    midiEventPacket_t rx;
+    do {
+      rx = MidiUSB.read();
+      if (rx.header != 0) {
+        if (rx.header == 9) {
+          trellis.setLED(findIndex(rx.byte2));
+        }
+        else {
+          trellis.clrLED(findIndex(rx.byte2));
+        }
+      }
+    } while (rx.header != 0);
+    trellis.writeDisplay();
+
+    prevReadTime = t;
+    digitalWrite(LED, ++heart & 32); // Blink = alive
+    MidiUSB.flush();
+  }
+}
